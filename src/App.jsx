543576e0296fa7c88721f10c5afce557ac5f4568
Bhavin B. Hariyani <Bhavin.Hariyani@amd.ibaset.com>
2018-01-30 22:57:50 +0530

    Removed unwanted files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import Grommet from 'grommet/components/Grommet';
 import Box from 'grommet/components/Box';
+import store from './stores';
 
-import Form from './components/form'
 
-const App = (props) => (   
-    <Box>
-        Hello World
-        <Form />
-    </Box>
+import { Provider } from 'react-redux';
+
+import { EmpList } from './components/EmployeeList';
+
+const App = (props) => (
+    <Grommet>
+        <Provider store={store}>
+            <Box flex>
+                <EmpList />
+            </Box>
+        </Provider>
+    </Grommet>
 );
 
 export default App;
